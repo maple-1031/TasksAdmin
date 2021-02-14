9b15ac647e41d8f7f91e997e5be95d3ded3aab7d
Kaede Oishi/windows <maple_1031@keio.jp>
2021-02-15 08:56:17 +0900

    .dbと.txtを使ったバージョン

--- server.py
+++ server.py
@@ -15,6 +15,11 @@ import requests
 import time
 from selenium import webdriver
 import sqlite3
+import os
+from pathlib import Path
+import sys
+import shutil
+import glob
 
 import settings
 
@@ -31,9 +36,38 @@ class Report():
         comm = f"select * from subs where subid = {subid}"
         i = tuple(c.execute(comm))
         return i[0][1]
+    
+class Task():
+    def __init__(self):
+        self.id = 0
+        self.path = ""
+        self.subject = ""
+        self.deadline = 0
+        
+    def id_gen(self, data):
+        return int(data)+1
+    
+def getLatestDownloadedFileName(arg):
+    p = Path(arg)
+    files = list(p.glob("*"))
+    file_updates = {file_path: os.stat(file_path).st_mtime for file_path in files}
+
+    newst_file_path = max(file_updates, key=file_updates.get)
+    return newst_file_path
+
+
+def dup_check(filename):
+    comm = f"select * from files where file='{filename}'"
+    if len(tuple(c.execute(comm))) == 0:
+        c.execute(f'insert into files values("{filename}")')
+        return False
+    else:
+        return True
 
 mail = settings.user
 password = settings.password
+
+downloadsFilePath = r'D:\Users\maple\OneDrive - keio.jp\★課題★\.temp'
 
 driver = webdriver.Chrome("D:\\Users\\maple\\Python\\chromedriver_win32\\chromedriver.exe")
 driver.get('https://portal.keio.jp/koid/')
@@ -61,26 +95,88 @@ time.sleep(0.5)
 report_button = driver.find_elements_by_xpath("//form[@action=\"/ess2/reportAction\"]/button")
 report_subid = driver.find_elements_by_xpath("//form[@action=\"/ess2/reportAction\"]/input[@name=\"SELECT_ENTNO\"]")
 
+report_list = []
+
 for i in range(len(report_button)):
+    report_temp = []
     report_button = driver.find_elements_by_xpath("//form[@action=\"/ess2/reportAction\"]/button")
     report_subid = driver.find_elements_by_xpath("//form[@action=\"/ess2/reportAction\"]/input[@name=\"SELECT_ENTNO\"]")
     report = Report()
     argid = int(report_subid[i].get_attribute("value"))
     report.name = report.subid2name(argid)
-    print(report.name)
     report_button[i].click()
     report.deadline = driver.find_element_by_xpath('//*[@id="student"]/div[2]/div[1]/form/div[1]/div/div/div/div[2]/div[5]/div/p').text
-    print(report.deadline)
+    temp = driver.find_element_by_xpath('//*[@id="student"]/div[2]/div[1]/form/div[1]/div/div/div/div[2]/div[1]/div/p').text
+
+    
+    if not dup_check(temp):
+        try:
+            driver.find_element_by_xpath('//*[@name="BTN__DL"]')
+        except:
+            report.filename = "#"
+        else:
+            driver.find_element_by_xpath('//*[@name="BTN__DL"]').click()
+            try:
+                driver.find_element_by_xpath('//button[@onclick="agreeSubmit("#_DL", "");"]').click()
+            except:
+                pass
+            else:
+                pass
+
+            timeout_second = 5
+
+            for i in range(timeout_second + 1):
+                download_fileName = glob.glob(f'{downloadsFilePath}\\*.*')
+
+                if download_fileName:
+                    extension = os.path.splitext(download_fileName[0])
+
+                    if '.crdownload' not in extension[1] : break
+
+                if i >= timeout_second:
+                    driver.quit()
+                    shutil.rmtree(downloadsFilePath)
+                    sys.exit()
+
+                time.sleep(1)
+
+            report.filename = getLatestDownloadedFileName(downloadsFilePath)
+    
+    report_temp = [report.name, report.deadline, report.filename]
+    report_list.append(report_temp)
+    
     driver.find_element_by_xpath('//button[@name="BTN_JIKANWARI"]').click()
     
+print(report_list)
 conn.close()
 driver.quit()
+
+conn = sqlite3.connect('current_tasks.db')
+c = conn.cursor()
+
+for i in report_list:
+    task = Task()
+    id_source = open('int.txt', 'r', encoding='UTF-8')
+    id_data = id_source.read()
+    id_source.close()
+    task.id = task.id_gen(id_data)
+    id_write = open("int.txt", "w")
+    id_write.write(str(task.id))
+    id_write.close()
+    
+    task.path = i[2]
+    task.deadline = int(task.deadline_gen(i[1]))
+    comm = f"insert into tasks values({task.id}, '{task.path}', '{i[0]}', {task.deadline}, '{i[1]}')"
+
+    c.execute(comm)
+
+conn.close()
 
 nc = Flask(__name__)
 
 @nc.route("/")
 def check():
-      rep_num = {"list": 3}
+      rep_num = {"list": 2}
       deadline = {0: 10, 1 :300}
       subject = {"val1": 5, "val2": 400}
       return render_template('index.html', deadline = deadline, subject = subject, rep_num = rep_num)
