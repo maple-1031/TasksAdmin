ed8916cdc9c602db362ba7c648870fc7f39d63c1
Kaede Oishi/windows <maple_1031@keio.jp>
2021-02-19 18:10:07 +0900

    コンテンツ内のflask対応

--- server.py
+++ server.py
@@ -81,128 +81,148 @@ def check():
     conn = sqlite3.connect('subs_2020.db')
     global c
     c = conn.cursor()
-    chromeOptions = webdriver.ChromeOptions()
-    chromeOptions.add_argument("--headless")
+    
     prefs = {"download.default_directory" : r"D:\Users\maple\OneDrive - keio.jp\★課題★\.temp"}
-    chromeOptions.add_experimental_option("prefs",prefs)
     
     mail = settings.user
-    password = settings.password
+    password = settings.password    
+    
+    chromeOptions = webdriver.ChromeOptions()
+#    chromeOptions.add_argument("--headless")
+    chromeOptions.add_experimental_option("prefs",prefs)
     
     downloadsFilePath = r'D:\Users\maple\OneDrive - keio.jp\★課題★\.temp'
     
-    chrome_path = "D:\\Users\\maple\\Python\\chromedriver_win32\\chromedriver.exe"
-    driver = webdriver.Chrome(executable_path=chrome_path, options=chromeOptions)
-    driver.get('https://portal.keio.jp/koid/')
-    time.sleep(1)
-    search_box = driver.find_element_by_id("username")
-    search_box.send_keys(mail)
-    pass_box = driver.find_element_by_id("password")
-    pass_box.send_keys(password)
-    submit_bottun = driver.find_element_by_xpath("/html/body/div/div[1]/div/div/form/section[3]/button")
-    submit_bottun.click()
-    time.sleep(0.5)
+    with open("recent_refresh.json") as f:
+        recent_refresh = json.load(f)
     
-    seminor_botton = driver.find_element_by_id("ui-id-12")
-    seminor_botton.click()
+    now = dt.now()
+    recent_refresh_time = dt.strptime(recent_refresh["recent_refresh"], "%Y-%m-%d %H:%M:%S")
     
-    seminor_suport_button = driver.find_element_by_id("ui-id-51")
-    seminor_suport_button.click()
-    
-    driver.switch_to.window(driver.window_handles[1])
-    time.sleep(0.5)
-    
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(0.5)
-    
-    report_button = driver.find_elements_by_xpath("//form[@action=\"/ess2/reportAction\"]/button")
-    report_subid = driver.find_elements_by_xpath("//form[@action=\"/ess2/reportAction\"]/input[@name=\"SELECT_ENTNO\"]")
-    
-    report_list = []
-    
-    for i in range(len(report_button)):
-        report_temp = []
+    if (now - recent_refresh_time).total_seconds() > 3600:
+        
+        with open("recent_refresh.json") as f:
+            ref_dic = json.load(f)
+            
+        ref_dic["recent_refresh"] = dt.now().strftime('%Y-%m-%d %H:%M:%S')
+        
+        with open("recent_refresh.json", "w") as f:
+            json.dump(ref_dic, f, indent=4)
+                
+        chrome_path = "D:\\Users\\maple\\Python\\chromedriver_win32\\chromedriver.exe"
+        driver = webdriver.Chrome(executable_path=chrome_path, options=chromeOptions)
+        driver.get('https://portal.keio.jp/koid/')
+        time.sleep(1)
+        search_box = driver.find_element_by_id("username")
+        search_box.send_keys(mail)
+        pass_box = driver.find_element_by_id("password")
+        pass_box.send_keys(password)
+        submit_bottun = driver.find_element_by_xpath("/html/body/div/div[1]/div/div/form/section[3]/button")
+        submit_bottun.click()
+        time.sleep(0.5)
+        
+        seminor_botton = driver.find_element_by_id("ui-id-12")
+        seminor_botton.click()
+        
+        seminor_suport_button = driver.find_element_by_id("ui-id-51")
+        seminor_suport_button.click()
+        
+        driver.switch_to.window(driver.window_handles[1])
+        time.sleep(0.5)
+        
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(0.5)
+        
         report_button = driver.find_elements_by_xpath("//form[@action=\"/ess2/reportAction\"]/button")
         report_subid = driver.find_elements_by_xpath("//form[@action=\"/ess2/reportAction\"]/input[@name=\"SELECT_ENTNO\"]")
-        report = Report()
-        argid = int(report_subid[i].get_attribute("value"))
-        report.name = report.subid2name(argid)
-        report_button[i].click()
-        report.deadline = driver.find_element_by_xpath('//*[@id="student"]/div[2]/div[1]/form/div[1]/div/div/div/div[2]/div[5]/div/p').text
-        temp = driver.find_element_by_xpath('//*[@id="student"]/div[2]/div[1]/form/div[1]/div/div/div/div[2]/div[1]/div/p').text
-    
-    
-        if not dup_check(temp):
-            try:
-                driver.find_element_by_xpath('//*[@name="BTN__DL"]')
-            except:
-                report.filename = "#"
-            else:
-                driver.find_element_by_xpath('//*[@name="BTN__DL"]').click()
+        
+        report_list = []
+        
+        for i in range(len(report_button)):
+            report_temp = []
+            report_button = driver.find_elements_by_xpath("//form[@action=\"/ess2/reportAction\"]/button")
+            report_subid = driver.find_elements_by_xpath("//form[@action=\"/ess2/reportAction\"]/input[@name=\"SELECT_ENTNO\"]")
+            report = Report()
+            argid = int(report_subid[i].get_attribute("value"))
+            report.name = report.subid2name(argid)
+            report_button[i].click()
+            report.deadline = driver.find_element_by_xpath('//*[@id="student"]/div[2]/div[1]/form/div[1]/div/div/div/div[2]/div[5]/div/p').text
+            temp = driver.find_element_by_xpath('//*[@id="student"]/div[2]/div[1]/form/div[1]/div/div/div/div[2]/div[1]/div/p').text
+        
+        
+            if not dup_check(temp):
                 try:
-                    driver.find_element_by_xpath('//button[@onclick="agreeSubmit("#_DL", "");"]').click()
+                    driver.find_element_by_xpath('//*[@name="BTN__DL"]')
                 except:
-                    pass
+                    report.filename = "#"
                 else:
-                    pass
+                    driver.find_element_by_xpath('//*[@name="BTN__DL"]').click()
+                    try:
+                        driver.find_element_by_xpath('//button[@onclick="agreeSubmit("#_DL", "");"]').click()
+                    except:
+                        pass
+                    else:
+                        pass
+        
+                    timeout_second = 5
+        
+                    for i in range(timeout_second + 1):
+                        download_fileName = glob.glob(f'{downloadsFilePath}\\*.*')
+        
+                        if download_fileName:
+                            extension = os.path.splitext(download_fileName[0])
+        
+                            if '.crdownload' not in extension[1] : break
+        
+                        if i >= timeout_second:
+                            driver.quit()
+                            shutil.rmtree(downloadsFilePath)
+                            sys.exit()
+        
+                        time.sleep(1)
+        
+                    report.filename = getLatestDownloadedFileName(downloadsFilePath)
+            report.reportname = temp
+        
+            report_temp = [report.name, report.deadline, report.reportname, report.filename]
+            report_list.append(report_temp)
+        
+            driver.find_element_by_xpath('//button[@name="BTN_JIKANWARI"]').click()
+        
+        conn.commit()
+        conn.close()
+        driver.quit()
+        
+        for i in report_list:
+            task = Task()
+            with open("current_tasks.json", mode="rt", encoding="utf-8") as f:
+                df = json.load(f)
+                
+        
+            task.path = i[3]
+            task.subject = i[0]
+            task.deadline = int(task.deadline_gen(i[1]))
+            task.deadline_str = i[1]
+            task.task_hash = hash("".join(i[:3]))
+            temp_dict = {"path":task.path,
+                         "subject":task.subject,
+                         "deadline_unix":task.deadline,
+                         "deadline_str":task.deadline_str,
+                         "deadline_day_str":task.deadline_str.split(" ")[0],
+                         "task_hash":task.task_hash
+                         }
+            if temp_dict not in list(df.values())[0]:
+                df["current_tasks"].append(temp_dict)
+        
+            with open("current_tasks.json", mode="wt", encoding="utf-8") as f:
+                json.dump(df, f, indent=4, ensure_ascii=False)
+        
+        #ここまで
     
-                timeout_second = 5
+    with open("current_tasks.json", mode="rt", encoding="utf-8") as f:
+        current_tasks = json.load(f)
     
-                for i in range(timeout_second + 1):
-                    download_fileName = glob.glob(f'{downloadsFilePath}\\*.*')
-    
-                    if download_fileName:
-                        extension = os.path.splitext(download_fileName[0])
-    
-                        if '.crdownload' not in extension[1] : break
-    
-                    if i >= timeout_second:
-                        driver.quit()
-                        shutil.rmtree(downloadsFilePath)
-                        sys.exit()
-    
-                    time.sleep(1)
-    
-                report.filename = getLatestDownloadedFileName(downloadsFilePath)
-        report.reportname = temp
-    
-        report_temp = [report.name, report.deadline, report.reportname, report.filename]
-        report_list.append(report_temp)
-    
-        driver.find_element_by_xpath('//button[@name="BTN_JIKANWARI"]').click()
-    
-    conn.commit()
-    conn.close()
-    driver.quit()
-    
-    for i in report_list:
-        task = Task()
-        with open("current_tasks.json", mode="rt", encoding="utf-8") as f:
-            df = json.load(f)
-    
-        task.path = i[3]
-        task.subject = i[0]
-        task.deadline = int(task.deadline_gen(i[1]))
-        task.deadline_str = i[1]
-        task.task_hash = hash("".join(i[:3]))
-        temp_dict = {"path":task.path,
-                     "subject":task.subject,
-                     "deadline_unix":task.deadline,
-                     "deadline_str":task.deadline_str,
-                     "deadline_day_str":task.deadline_str.split(" ")[0],
-                     "task_hash":task.task_hash
-                     }
-        if temp_dict not in list(df.values())[0]:
-            df["current_tasks"].append(temp_dict)
-    
-        with open("current_tasks.json", mode="wt", encoding="utf-8") as f:
-            json.dump(df, f, indent=4, ensure_ascii=False)
-    
-
-    rep_num = {"list": 2}
-    deadline = {0: 10, 1 :300}
-    subject = {"val1": 5, "val2": 400}
-    return render_template('index.html', deadline = deadline, subject = subject, rep_num = rep_num)
+    return render_template('index.html', ct = current_tasks, rep_num = len(current_tasks["current_tasks"]))
 
 if __name__ == '__main__':
     nc.run(host="0.0.0.0", port=8080, debug=False)
